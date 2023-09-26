5743408cd451781d41ec3dbfa80847e6d4198f5b
Redsilver2 <Marvintanonkou@gmail.com>
2023-09-26 10:37:39 -0400

    Change Metric.cs

--- Program/Metric.cs
+++ Program/Metric.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class Metric
 {
 	public Metric()
 	{
-		Console.WriteLine("Hello world");
-	}
+
+		string measure1, measure2;
+
+        while (true)
+		{
+            Console.WriteLine("Entrer une mesure que vous utilisez (mm, cm, dm, m ou km)."); 
+			measure1 = Console.ReadLine();
+
+			switch (measure1.ToLower())
+			{
+				case "mm":
+					break;
+
+                case "cm":
+                    break;
+
+                case "dm":
+                    break;
+
+                case "m":
+                    break;
+
+                case "km":
+                    break;
+
+
+                default:
+
+					break;
+
+
+            }
+
+        }
+    }
 }
