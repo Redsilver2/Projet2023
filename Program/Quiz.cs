a139bf267ad5c9c233b8c64de0305ee9a5808c3f
Jimmy Comeault <jimmy.comeault@gmail.com>
2023-09-26 10:51:06 -0400

    End Quiz

--- Program/Quiz.cs
+++ Program/Quiz.cs
@@ -12,22 +12,89 @@ namespace Program
         public Quiz()
         {
 
-            
+
             {
+
+                string? sRep;
 
 
                 Console.WriteLine("Connaissez-vous vos capitale?");
 
+                Console.WriteLine("Question 1 : Quel est la capitale de la France?");
+                sRep = Console.ReadLine();
+                if (sRep == "Paris" || sRep == "paris")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
+
+
+
+                Console.WriteLine("Question 2 : Quel est la capitale de l'Espagne?");
+                sRep = Console.ReadLine();
+                if (sRep == "Madrid" || sRep == "madird")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
+
+
+
+                Console.WriteLine("Question 3 : Quel est la capitale de la Grèce?");
+                sRep = Console.ReadLine();
+                if (sRep == "Athènes" || sRep == "athènes" || sRep == "Athenes" || sRep == "athene")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
 
 
 
 
+                Console.WriteLine("Question 4 : Quel est la capitale des Pays-Bas?");
+                sRep = Console.ReadLine();
+                if (sRep == "Amsterdam" || sRep == "amsterdam")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
 
 
+                Console.WriteLine("Question 5 : Quel est la capitale du Royaume-Uni?");
+                sRep = Console.ReadLine();
+                if (sRep == "Londres" || sRep == "londre")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
 
 
-            } 
+                Console.WriteLine("Question 6 : Quel est la capitale de l'Allemange?");
+                sRep = Console.ReadLine();
+                if (sRep == "Berlin" || sRep == "berlin")
+                {
+                    Console.WriteLine("Bonne Réponse!");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse...");
+                }
+            }
         }
     }
-    
 }
